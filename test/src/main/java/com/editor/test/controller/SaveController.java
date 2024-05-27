f05113500da132a0d86f16bf51e8baf5babc2de3
Kimbumsoo99 <show7441@naver.com>
2024-05-27 23:58:36 +0900

    feat: SaveDto 추가

--- test/src/main/java/com/editor/test/controller/SaveController.java
+++ test/src/main/java/com/editor/test/controller/SaveController.java
@@ -1,7 +1,25 @@
 package com.editor.test.controller;
 
+import com.editor.test.model.dto.SaveDto;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class SaveController {
+
+    private static final Logger log = LoggerFactory.getLogger(SaveController.class);
+
+    @PostMapping("/save")
+    public String saveFunc(SaveDto saveDto){
+        log.debug("SaveDto - {}", saveDto);
+        System.out.println("saveDto = " + saveDto);
+
+        return "redirect:/";
+    }
 }
---
+++ test/src/main/java/com/editor/test/model/dto/SaveDto.java
@@ -0,0 +1,12 @@
+package com.editor.test.model.dto;
+
+import lombok.*;
+
+// Getter, Setter, Constructor 등 다 포함
+@Data
+public class SaveDto {
+
+    private String title;
+    private String content;
+
+}
