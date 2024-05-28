57abc8e997bbe0e9dbb309e2394bc648770bef9a
Kimbumsoo99 <show7441@naver.com>
2024-05-28 21:10:12 +0900

    feat: DB 연결 성공 & 인코딩 문제 발생

---
+++ test/src/main/java/com/editor/test/config/WebConfig.java
@@ -0,0 +1,8 @@
+package com.editor.test.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+}
--- test/src/main/java/com/editor/test/controller/SaveController.java
+++ test/src/main/java/com/editor/test/controller/SaveController.java
@@ -5,19 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class SaveController {
 
-    private static final Logger log = LoggerFactory.getLogger(SaveController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SaveController.class);
 
     @PostMapping("/save")
     public String saveFunc(SaveDto saveDto){
-        log.debug("SaveDto - {}", saveDto);
+        LOG.debug("SaveDto - {}", saveDto);
         System.out.println("saveDto = " + saveDto);
 
         return "redirect:/";
