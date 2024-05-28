00ac410f3ee6622855aeed9f416db0b08f05fc4b
Kimbumsoo99 <show7441@naver.com>
2024-05-29 00:13:52 +0900

    feat: DB table 생성 (RDS 활용)
    - entity 추가로 자동 테이블 생성
    - application.properties 확인해보면 다음 내용이 추가 돼있음.
    ```
    #테이블 생성 조건
    spring.jpa.hibernate.ddl-auto=update
    
    #테이블 변수 작명법 및 변경법
    spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl
    ```

---
+++ test/src/main/java/com/editor/test/repository/ContentRepository.java
@@ -0,0 +1,10 @@
+package com.editor.test.repository;
+
+import com.editor.test.entity.ContentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentRepository extends JpaRepository<ContentEntity, Integer> {
+    // Entity와 Entity의 id에 해당하는 타입
+    // interface로 선언하면, @Repository를 하지 않아도된다. -> 해도 상관 무
+
+}
