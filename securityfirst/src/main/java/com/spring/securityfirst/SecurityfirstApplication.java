5ba2b26ad6c45bd7092b295847a564750721697a
Kimbumsoo99 <show7441@naver.com>
2024-06-08 03:59:41 +0900

    init : security project init

---
+++ securityfirst/src/main/java/com/spring/securityfirst/SecurityfirstApplication.java
@@ -0,0 +1,13 @@
+package com.spring.securityfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityfirstApplication.class, args);
+	}
+
+}
