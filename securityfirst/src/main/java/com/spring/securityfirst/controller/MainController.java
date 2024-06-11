bc8bce6e314d84f89a96fa738c2bdf055c12f9a8
Kimbumsoo99 <show7441@naver.com>
2024-06-11 10:20:40 +0900

    feat : 로그인 된 사용자 정보 확인하기
    - SecurityContextHolder

--- securityfirst/src/main/java/com/spring/securityfirst/controller/MainController.java
+++ securityfirst/src/main/java/com/spring/securityfirst/controller/MainController.java
@@ -1,13 +1,38 @@
 package com.spring.securityfirst.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.asm.IModelFilter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 @Controller
+@Slf4j
 public class MainController {
 
     @GetMapping("/")
-    public String mainP(){
+    public String mainP(Model model){
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> it = authorities.iterator();
+        GrantedAuthority auth = it.next();
+        String role = auth.getAuthority();
+
+        log.debug("====== MainController mainP ======");
+        log.debug("id - {}", id);
+        log.debug("role - {}", role);
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
 
         return "main";
     }
