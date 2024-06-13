dcea51a260a7a3dcfc0499f316f41c6c2d0315fc
Kimbumsoo99 <show7441@naver.com>
2024-06-13 11:03:27 +0900

    feat: JPA 설정
    - UserEntity 생성
    - UserRepository 생성

---
+++ jwt/src/main/java/com/security/jwt/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.security.jwt.repository;
+
+import com.security.jwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
