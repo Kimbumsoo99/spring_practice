983aa380e15ae6b1264256c637bbd59078e4ceba
Kimbumsoo99 <show7441@naver.com>
2024-06-13 11:43:19 +0900

    feat: Join Process Implementation

---
+++ jwt/src/main/java/com/security/jwt/controller/JoinController.java
@@ -0,0 +1,27 @@
+package com.security.jwt.controller;
+
+import com.security.jwt.model.dto.JoinDTO;
+import com.security.jwt.model.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @Autowired
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    private String joinProcess(JoinDTO joinDTO){
+        return "ok";
+    }
+}
