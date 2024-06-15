efb896e4383df8102abff6c0e6b0547a61b9ccb2
Kimbumsoo99 <show7441@naver.com>
2024-06-15 20:04:31 +0900

    init

---
+++ oauthsession/src/main/java/com/spring/oauthsession/OauthsessionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.oauthsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthsessionApplication.class, args);
+	}
+
+}
