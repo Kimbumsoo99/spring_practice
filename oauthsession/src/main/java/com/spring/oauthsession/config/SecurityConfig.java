f9c8e9a64324dd401678d81e9c908c41807138a6
Kimbumsoo99 <show7441@naver.com>
2024-06-21 04:09:55 +0900

    feat: OAuth2 Configuration Class 기반으로 설정 변경

--- oauthsession/src/main/java/com/spring/oauthsession/config/SecurityConfig.java
+++ oauthsession/src/main/java/com/spring/oauthsession/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring.oauthsession.config;
 
+import com.spring.oauthsession.oauth2.CustomClientRegistrationRepo;
 import com.spring.oauthsession.service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomClientRegistrationRepo customClientRegistrationRepo;
 
     @Autowired
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepo customClientRegistrationRepo) {
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customClientRegistrationRepo = customClientRegistrationRepo;
     }
 
     @Bean
@@ -29,6 +32,7 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .oauth2Login((oauth2) -> oauth2
                         .loginPage("/login")
+                        .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
                         .userInfoEndpoint(userInfoEndpointConfig ->
                                 userInfoEndpointConfig.userService(customOAuth2UserService)))
                 .authorizeHttpRequests((auth) -> auth
@@ -36,5 +40,5 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .build();
     }
-    
+
 }
