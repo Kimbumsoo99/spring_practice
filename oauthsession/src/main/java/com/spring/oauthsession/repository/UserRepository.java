aa513ce1c2a3f75a951403076cf8db9835edc291
Kimbumsoo99 <show7441@naver.com>
2024-06-20 18:09:02 +0900

    feat: UserRepository 구현, oauth2 계정 save 구문 추가

---
+++ oauthsession/src/main/java/com/spring/oauthsession/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.spring.oauthsession.repository;
+
+import com.spring.oauthsession.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
--- oauthsession/src/main/java/com/spring/oauthsession/service/CustomOAuth2UserService.java
+++ oauthsession/src/main/java/com/spring/oauthsession/service/CustomOAuth2UserService.java
@@ -4,6 +4,9 @@ import com.spring.oauthsession.dto.CustomOAuth2User;
 import com.spring.oauthsession.dto.GoogleResponse;
 import com.spring.oauthsession.dto.NaverResponse;
 import com.spring.oauthsession.dto.OAuth2Response;
+import com.spring.oauthsession.entity.UserEntity;
+import com.spring.oauthsession.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -12,6 +15,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
         OAuth2User oAuth2User = super.loadUser(userRequest);
         System.out.println(oAuth2User.getAttributes());
@@ -27,7 +38,27 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        String role = "ROLE_USER";
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
+
+        String role = null;
+
+        if (existData == null) {
+            UserEntity user = new UserEntity();
+            user.setUsername(username);
+            user.setEmail(oAuth2Response.getEmail());
+            user.setRole("ROLE_USER");
+
+            userRepository.save(user);
+        } else {
+            existData.setUsername(username);
+            existData.setEmail(oAuth2Response.getEmail());
+
+            role = existData.getRole();
+
+            userRepository.save(existData);
+        }
+
         return new CustomOAuth2User(oAuth2Response, role);
     }
 
