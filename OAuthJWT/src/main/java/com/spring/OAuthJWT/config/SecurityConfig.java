c25a533879b6cc6b6c3d93e855a6a5260de4f03c
Kimbumsoo99 <show7441@naver.com>
2024-06-30 03:15:00 +0900

    feat: Cors 설정 추가

--- OAuthJWT/src/main/java/com/spring/OAuthJWT/config/SecurityConfig.java
+++ OAuthJWT/src/main/java/com/spring/OAuthJWT/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.spring.OAuthJWT.jwt.JWTFilter;
 import com.spring.OAuthJWT.jwt.JWTUtil;
 import com.spring.OAuthJWT.oauth2.CustomSuccessHandler;
 import com.spring.OAuthJWT.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,10 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +38,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
+                .cors(corsCustomizer -> corsCustomizer
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true);
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+
+                                configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return null;
+                            }
+                        }))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
@@ -49,4 +72,5 @@ public class SecurityConfig {
                 .build();
     }
 
+    f
 }
