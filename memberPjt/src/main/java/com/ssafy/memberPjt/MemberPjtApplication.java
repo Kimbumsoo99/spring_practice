8892a82f71a8b07478ec1209dd78a6a02eaf1e4a
Kimbumsoo99 <show7441@naver.com>
2024-07-05 09:35:00 +0900

    member project init

---
+++ memberPjt/src/main/java/com/ssafy/memberPjt/MemberPjtApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.memberPjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberPjtApplication.class, args);
+	}
+
+}
---
+++ memberPjt/src/test/java/com/ssafy/memberPjt/MemberPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.memberPjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
