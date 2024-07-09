4db5f142b3868a6a7a2fcc90b86f334163db85f8
Kimbumsoo99 <show7441@naver.com>
2024-07-09 17:56:44 +0900

    feat: Todo 시스템 생성

--- front-server/react-workspace/todo-react-app/src/App.js
+++ front-server/react-workspace/todo-react-app/src/App.js
@@ -1,29 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Todo from "./Todo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, List, Paper } from "@mui/material";
 import AddTodo from "./AddTodo";
+import { call } from "./AppService";
 
 function App() {
-    const [items, setItem] = useState([
-        {
-            id: "0",
-            title: "Hello World 1",
-            done: true,
-        },
-        {
-            id: "1",
-            title: "Hello World 2",
-            done: false,
-        },
-    ]);
+    const [items, setItem] = useState([]);
+
+    useEffect(() => {
+        call("/todo", "GET", null).then((res) => setItem(res.data));
+    }, []);
+
+    const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    };
+
+    const addItem = (item) => {
+        call("/todo", "POST", item).then((res) => setItem(res.data));
+    };
+
+    const deleteItem = (item) => {
+        call("/todo", "DELETE", item).then((res) => setItem(res.data));
+    };
+
+    const editItem = (item) => {
+        call("/todo", "PUT", item).then((res) => setItem(res.data));
+    };
 
     let todoItems = items.length > 0 && (
         <Paper style={{ margin: 16 }}>
             <List>
                 {items.map((item) => (
-                    <Todo item={item} key={item.id} />
+                    <Todo item={item} key={item.id} editItem={editItem} deleteItem={deleteItem} />
                 ))}
             </List>
         </Paper>
@@ -31,7 +42,7 @@ function App() {
     return (
         <div className="App">
             <Container maxWidth="md">
-                <AddTodo />
+                <AddTodo addItem={addItem} />
                 <div className="TodoList">{todoItems}</div>
             </Container>
         </div>
--- front-server/react-workspace/todo-react-app/src/Todo.js
+++ front-server/react-workspace/todo-react-app/src/Todo.js
@@ -1,22 +1,66 @@
 import React, { useState } from "react";
-import { ListItem, ListItemText, InputBase, Checkbox } from "@mui/material";
+import {
+    ListItem,
+    ListItemText,
+    InputBase,
+    Checkbox,
+    ListItemSecondaryAction,
+    IconButton,
+} from "@mui/material";
+import { DeleteOutline } from "@mui/icons-material";
 
 const Todo = (props) => {
     const [item, setItem] = useState(props.item);
+    const [readOnly, setReadOnly] = useState(true);
+
+    const deleteItem = props.deleteItem;
+
+    const deleteEventHandler = () => {
+        deleteItem(item);
+    };
+
+    const turnOffReadOnly = () => {
+        setReadOnly(false);
+    };
+
+    const editItem = props.editItem;
+
+    const editEventHandler = (e) => {
+        setItem({ ...item, title: e.target.value });
+    };
+    const turnOnReadOnly = (e) => {
+        if (e.key === "Enter" && readOnly === false) {
+            setReadOnly(true);
+            editItem(item);
+        }
+    };
+    const checkboxEventHandler = (e) => {
+        item.done = e.target.checked;
+        editItem(item);
+    };
 
     return (
         <ListItem>
-            <Checkbox checked={item.done} />
+            <Checkbox checked={item.done} onChange={checkboxEventHandler} />
             <ListItemText>
                 <InputBase
-                    inputProps={{ "aria-label": "naked" }}
+                    inputProps={{ "aria-label": "naked", readOnly: readOnly }}
+                    onClick={turnOffReadOnly}
+                    onKeyDown={turnOnReadOnly}
+                    onChange={editEventHandler}
                     type="text"
                     id={item.id}
+                    name={item.id}
                     value={item.title}
                     multiline={true}
                     fullWidth={true}
                 />
             </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Delete Todo" onClick={deleteEventHandler}>
+                    <DeleteOutline />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     );
 };
