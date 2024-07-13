14b93220229fea148f2e7fb3a99c7ea0af95ae24
Kimbumsoo99 <show7441@naver.com>
2024-07-13 21:32:57 +0900

    배포 관련 코드 추가

--- reacttest/src/main/java/com/project/reacttest/controller/HealthCheckController.java
+++ reacttest/src/main/java/com/project/reacttest/controller/HealthCheckController.java
@@ -1,2 +1,10 @@
-package com.project.reacttest.controller;public class HealthCheckController {
+package com.project.reacttest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HealthCheckController {
+    @GetMapping("/")
+    public String healthCheck(){
+        return "The service is up and running...";
+    }
 }
